cf5a001c9e8b641780eb9fddc3ec7c23424569c2
yongrae Kim <kyr9389@naver.com>
2020-12-28 23:19:38 +0900

    [update] drag detect function added
    Signed-off-by: yongrae Kim <kyr9389@naver.com>

--- public/background.js
+++ public/background.js
@@ -6,6 +6,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     alert(changeInfo.url);
 });
 
+
 /*
 chrome.tabs.onActivated.addListener(function(activeInfo) {
     // how to fetch tab url using activeInfo.tabid
--- public/getSource.js
+++ public/getSource.js
@@ -3,6 +3,7 @@
 console.log("immediate function test");
 
 function get_source(document_body) {
+    console.log("immediate function test22");
     return document_body.innerText;
 }
 
@@ -10,3 +11,24 @@ chrome.extension.sendMessage({
     action: "getSource",
     source: get_source(document.body)
 });
+
+function dragText() {
+    console.log("mouse move");
+
+    let text;
+
+    if(window.getSelection) {
+        text = window.getSelection().toString();
+    }
+    else if (document.selection) {
+        text = window.selection.createRange().texst;
+    }
+    return text;
+}
+
+document.onmouseup = function() {
+    if(dragText()) {
+        alert(dragText());
+        console.log("working");
+    }
+}
